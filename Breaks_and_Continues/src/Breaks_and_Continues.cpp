e19f4c35b8abd886e4129b6ba8808f2231e2fa92
Kevin Hallman <musicturtul@192.168.1.103>
2018-06-02 03:05:27 -0400

    Made for loops with breaks and continues to learn

--- Breaks_and_Continues/src/Breaks_and_Continues.cpp
+++ Breaks_and_Continues/src/Breaks_and_Continues.cpp
@@ -10,6 +10,24 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+//breaks will cause a stop to the loop and it will then go to the next part
+	for (int i = 1; i < 5; i++) {
+		cout << "i is: " << i << endl;
+		if (i == 3) {
+			break;
+		}
+		cout << "Logging..." << endl;
+	}
+	cout << "The break has stopped my logging..." << endl;
+
+//continue will not completely stop the loop, but it will go on to the next part of the loop without finishing the rest
+	for (int i = 1; i < 5; i++) {
+		cout << "i is now: " << i << endl;
+		if (i == 3) {
+			continue;
+		}
+		cout << "Logging i still..." << endl;
+	}
+	cout << "It did not log after i was equal to 3 because of the continue" << endl;
 	return 0;
 }
